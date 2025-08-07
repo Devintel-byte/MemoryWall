8effd496aad18cde078e78c0245ee6d9fef53b82
sincelectron <joemugbeh@gmail.com>
2025-08-07 02:10:51 +0100

    added url origin

--- socket-server/index.ts
+++ socket-server/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["https://your-vercel-app.vercel.app", "http://localhost:3000"],
+    origin: ["https://memory-wall-two.vercel.app/", "http://localhost:3000"],
     methods: ["GET", "POST"]
   }
 });
